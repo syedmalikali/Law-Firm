dd4ac74b6da7c58fcc923bfc0338aaf60a23f398
Syed Malik Ali <syedmalikali@gmail.com>
2023-09-17 15:32:57 +0300

    Matter  Commit

---
+++ law_firm/law_firm/doctype/matter/matter.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Syed Malik Ali and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Matter", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ law_firm/law_firm/doctype/matter/test_matter.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Syed Malik Ali and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMatter(FrappeTestCase):
+	pass
