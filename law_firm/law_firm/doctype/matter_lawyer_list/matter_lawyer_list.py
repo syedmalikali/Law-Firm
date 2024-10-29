03243af67ffb3f8646c0c84695a38d18d3ed933c
Nellai <erpnellai@vps-f5bcd7f3.vps.ovh.ca>
2024-10-29 10:03:41 +0300

    2024

---
+++ law_firm/law_firm/doctype/matter_lawyer_list/matter_lawyer_list.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Syed Malik Ali and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class MatterLawyerList(Document):
+	pass
