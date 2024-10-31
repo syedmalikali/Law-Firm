f61d48085295ab5b212a74b5a2109c9cfba6da14
Nellai <erpnellai@vps-f5bcd7f3.vps.ovh.ca>
2024-10-31 10:10:29 +0300

    LF timesheet

---
+++ law_firm/law_firm/doctype/lf_timesheet/lf_timesheet.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Syed Malik Ali and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("LF Timesheet", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ law_firm/law_firm/doctype/lf_timesheet/test_lf_timesheet.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Syed Malik Ali and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLFTimesheet(FrappeTestCase):
+	pass
