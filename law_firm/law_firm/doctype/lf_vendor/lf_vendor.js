874e5e81bd0015ec37f46c45c7c38eba6454270a
Syed Malik Ali <syedmalikali@vmi2019463.contaboserver.net>
2025-01-22 14:54:42 +0300

    workspace

---
+++ law_firm/law_firm/doctype/lf_vendor/lf_vendor.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Syed Malik Ali and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("LF Vendor", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ law_firm/law_firm/doctype/lf_vendor/test_lf_vendor.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Syed Malik Ali and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLFVendor(FrappeTestCase):
+	pass
